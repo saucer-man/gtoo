a46d16997c7c1404d644d8eb6a8c3a6549d39155
yanqing.geng <yanqing.geng@yitu-inc.com>
2020-11-13 21:01:26 +0800

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # gtoo
 penetration tools based golang
+
+渗透测试中的脚手架工具。不断更新中
+
+todo
+- 编码部分
+    - [x] base64编码/解码
+    - [ ] md5
+
+- 漏洞扫描
+    - [x] 端口探测
+
+- 内网
+    - [ ] 存活探测
+    - [ ] MS17010
--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/spf13/cobra"
 )
 
@@ -8,6 +10,18 @@ var rootCmd = &cobra.Command{
 	Use:   "gtoo",
 	Short: "gtoo id a pentese tool",
 	Long:  `gtoo id a pentese tool, when you have this, you will got all`,
+}
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "version of gtoo",
+	Long:  `version of gtoo`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("gtoo version: v0.1")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
 }
 
 func Execute() {
