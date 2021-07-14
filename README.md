219b9c58b3c59e6297dc10939db1f9e1fd57b5e6
saucer-man <gyq0929@gmail.com>
2021-07-14 19:10:31 +0800

    去除配置依赖

--- README.md
+++ README.md
@@ -3,38 +3,17 @@ penetration tools based golang
 
 渗透测试中的脚手架工具，基本原则是无任何依赖，开箱即用。
 
-todo
-- 编码部分
-    - [x] base64编码/解码
-    - [x] md5
-
-- 漏洞扫描
-    - [x] 端口探测
-
-- 信息收集
-    - [x] whois查询/备案信息查询
-    - [ ] 子域名收集(非爆破)
-
-- 目标收集
-    - [x] zoomeye
-
-- 内网
-    - [ ] 存活探测
-    - [ ] MS17010
-```                   
-$ gtoo                           
+```bash
 gtoo id a pentese tool, when you have this, you will got all
 
 Usage:
   gtoo [command]
 
 Available Commands:
-  collect     collect domain and ip
-  convert     Common encoding and decoding
-  domain      search some domain info
+  convert     常用编码解码
+  domain      域名相关信息
   help        Help about any command
-  ip          search some domain info
-  portscan    scan open port of host
+  ip          ip相关信息
   version     version of gtoo
 
 Flags:
@@ -44,3 +23,41 @@ Use "gtoo [command] --help" for more information about a command.
 ```
 
 
+### 1. 域名信息
+
+```bash
+$ gtoo domain info baidu.com
+2021-07-14 19:06:01 [INFO] whois查询结果:
+注册人: MarkMonitor Inc.
+注册邮箱: abusecomplaints@markmonitor.com
+注册商: whois.markmonitor.com
+注册时间: 1999-10-11T11:05:17Z
+到期时间: 2026-10-11T11:05:17Z
+2021-07-14 19:06:01 [INFO] IPC查询结果:
+域名: baidu.com
+网站备案名称: 百度
+主办单位性质: 企业
+主办单位性质: 北京百度网讯科技有限公司
+备案许可证号: 京ICP证030173号-1
+最新审核检测: 2021-07-14 18:03:43
+````
+
+### 2. ip信息
+
+```bash
+$ gtoo ip info 120.78.49.231 --threadbookapikey xxxxx
+2021-07-14 19:09:41 [INFO] 微步在线查询IP信息...
+IP: 120.78.49.231
+情报可信度: 低
+是否为恶意IP: true
+IP危害等级: 低
+IP威胁类型: [IDC服务器 扫描]
+应用场景: 数据中心
+IP安全事件0
+    标签: [阿里云主机]
+    类别: public_info
+地理位置: 中国 广东省 深圳市
+经纬度: lng：114.014495 lat：22.542702
+运营商: 阿里云
+情报更新时间: 2021-07-12 23:04:45
+```
--- cmd/root.go
+++ cmd/root.go
@@ -1,16 +1,172 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
+	"gtoo/convert"
+	"gtoo/domain"
+	"gtoo/ip"
+	"gtoo/utils"
+	"net"
+	"os"
+	"strings"
 
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
+
+var ipCmd = &cobra.Command{
+	Use:   "ip",
+	Short: "ip相关信息",
+}
+var ThreadBookAPIKey string
+var ipInfoCmd = &cobra.Command{
+	Use: "info",
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) < 1 {
+			return errors.New("requires at least args\nExample: gtoo ip info example.com")
+		}
+		return nil
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		//检查ip地址是否正确
+		// ParseIP 这个方法 可以用来检查 ip 地址是否正确，如果不正确，该方法返回 nil
+		address := net.ParseIP(args[0])
+		if address == nil {
+			log.Error("ip地址格式不正确")
+			os.Exit(-1)
+		}
+		// } else {
+		// 	fmt.Println("正确的ip地址", address.String())
+		// }
+		//下面调用微步在线的api查询ip信誉
+
+		if ThreadBookAPIKey != "" {
+			t := ip.NewThreatBook(ThreadBookAPIKey)
+			log.Info("微步在线查询IP信息...")
+			err := t.IP(address.String())
+			if err != nil {
+				log.Errorf("微步在线查询IP信息失败: %v", err)
+			}
+		}
+		// TODO is cdn
+	},
+}
+
+func init() {
+	ipInfoCmd.PersistentFlags().StringVarP(&ThreadBookAPIKey, "threadbookapikey", "", "", "微步在线apikey")
+	rootCmd.AddCommand(ipCmd)
+	ipCmd.AddCommand(ipInfoCmd)
+}
+
+var convertCmd = &cobra.Command{
+	Use:   "convert",
+	Short: "常用编码解码",
+}
+
+var base64encodeCmd = &cobra.Command{
+	Use: "base64encode",
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) < 1 {
+			return errors.New("requires at least args\nExample: gtoo convert base64encode str_to_encode")
+		}
+		return nil
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		str := convert.Base64encode([]byte(args[0]))
+		fmt.Println(str)
+	},
+}
+var base64decodeCmd = &cobra.Command{
+	Use: "base64decode",
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) < 1 {
+			return errors.New("requires at least args\nExample: gtoo convert base64encode str_to_decode")
+		}
+		return nil
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		data, err := convert.Base64decode(args[0])
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(string(data))
+	},
+}
+
+var md5Cmd = &cobra.Command{
+	Use: "md5encode",
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) < 1 {
+			return errors.New("requires at least args\nExample: gtoo convert base64encode str_to_encode")
+		}
+		return nil
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		if utils.Exists(args[0]) {
+			data, err := convert.Md5encodeFile(args[0])
+			if err != nil {
+				panic(err)
+			}
+			fmt.Printf("%s file's md5 is %s\n", args[0], data)
+		} else {
+			data, err := convert.Md5encodeStr(args[0])
+			if err != nil {
+				panic(err)
+			}
+			fmt.Printf("%s str's md5 is %s\n", args[0], data)
+		}
+	},
+}
+
+var domainCmd = &cobra.Command{
+	Use:   "domain",
+	Short: "域名相关信息",
+}
+var domaininfoCmd = &cobra.Command{
+	Use: "info",
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) < 1 {
+			return errors.New("requires at least args\nExample: gtoo domain info example.com")
+		}
+		return nil
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		d := args[0]
+		if !strings.HasPrefix(d, "http") {
+			d = "https://" + d
+		}
+		err := domain.Whois(d)
+		if err != nil {
+			log.Errorf("whois查询出错: %v", err)
+		}
+		err = domain.Ipc(d)
+		if err != nil {
+			log.Errorf("IPC备案查询出错: %v", err)
+		}
+		// TODO is cdn
+		// TODO 威胁情报
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(domainCmd)
+	domainCmd.AddCommand(domaininfoCmd)
+}
 
 var rootCmd = &cobra.Command{
 	Use:   "gtoo",
 	Short: "gtoo id a pentese tool",
 	Long:  `gtoo id a pentese tool, when you have this, you will got all`,
 }
+
+func init() {
+	rootCmd.AddCommand(convertCmd)
+	convertCmd.AddCommand(base64encodeCmd)
+	convertCmd.AddCommand(base64decodeCmd)
+	convertCmd.AddCommand(md5Cmd)
+}
+
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "version of gtoo",
